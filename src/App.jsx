c84ec6adcb64d2b777cffab1931f984d533f0016
sergey <serjjiniuss@gmail.com>
2025-05-08 15:29:40 +0400

    Add Router example with data loaders, error boundaries, and useLoaderData

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Category from "./pages/Category";
+import ProductDetails from "./pages/ProductDetails";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+import Thanks from "./pages/Thanks";
+import fetchCategoryLoader from "./loaders/categoryLoader";
+import fetchProductLoader from "./loaders/productsLoader";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+                loader: fetchCategoryLoader,
+                errorElement: <ErrorBoundary />,
+            },
+            { path: "old-home", element: <Navigate to={"/"} /> },
+            { path: "about", element: <About /> },
+            { path: "cart", element: <Cart /> },
+            { path: "thanks", element: <Thanks /> },
+            {
+                path: "category/:categoryId",
+                element: <Category />,
+                loader: fetchProductLoader,
+                errorElement: <ErrorBoundary />,
+            },
+            { path: "product/:productId", element: <ProductDetails /> },
+            { path: "*", element: <NotFound /> },
+            // { path: "*", element: <Navigate to="/" /> },
+        ],
+    },
+]);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Header />
+//         <Home />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "about",
+//     element: (
+//       <>
+//         <Header />
+//         <About />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "cart",
+//     element: (
+//       <>
+//         <Header /> <Cart />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "categories",
+//     element: (
+//       <>
+//         <Header />
+//         <Categories />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "product",
+//     element: (
+//       <>
+//         <Header />
+//         <ProductDetails />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "*",
+//     element: (
+//       <>
+//         <Header />
+//         <NotFound />
+//         <Footer />
+//       </>
+//     ),
+//   },
+// ]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,5 @@
+function ErrorBoundary() {
+    return <div>Errooooooooooooooooooooooor!</div>;
+}
+
+export default ErrorBoundary;
