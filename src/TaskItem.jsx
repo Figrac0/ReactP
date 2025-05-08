e2ce1582e64569f903b6a2a6fab189f1079d7889
sergey <serjjiniuss@gmail.com>
2025-05-08 15:33:24 +0400

    Add task list app using React Context with sorting and completed tasks

---
+++ src/TaskItem.jsx
@@ -0,0 +1,36 @@
+import { useTask } from "./TaskProvide";
+
+export default function TaskItem({ task }) {
+    const { deleteTask, completeTask, isOverdue } = useTask();
+    const { title, priority, deadline, id, completed } = task;
+
+    return (
+        <li
+            className={`task-item ${priority.toLowerCase()} ${
+                isOverdue ? "overdue" : ""
+            }`}>
+            <div className="task-info">
+                <div>
+                    {title} <strong>{priority}</strong>
+                </div>
+                <div className="task-deadline">
+                    Due: {new Date(deadline).toLocaleString()}
+                </div>
+            </div>
+            <div className="task-buttons">
+                {!completed && (
+                    <button
+                        className="complete-button"
+                        onClick={() => completeTask(id)}>
+                        Complete
+                    </button>
+                )}
+                <button
+                    className="delete-button"
+                    onClick={() => deleteTask(id)}>
+                    Delete
+                </button>
+            </div>
+        </li>
+    );
+}
